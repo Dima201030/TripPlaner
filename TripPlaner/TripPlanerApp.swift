08324724f11a419bbd9ec8c7923813b9052b4e44
dima <dima@kojem.ru>
2024-03-20 14:18:28 +0300

    Initial Commit

---
+++ TripPlaner/TripPlanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripPlanerApp.swift
+//  TripPlaner
+//
+//  Created by Дима Кожемякин on 20.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TripPlanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
